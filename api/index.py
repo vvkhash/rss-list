2b01ad019858f2c40441bf2a1dd723378f5d0f21
Valeriy Khashkovskiy <valeriy.khashkovskiy1@lseg.com>
2024-09-17 23:00:03 +0200

    adds html text

--- api/index.py
+++ api/index.py
@@ -6,9 +6,11 @@ from api.rss import get_rss as get_feed
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        # self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write('<p>Hello, world!</p>'.encode('utf-8'))
+        self.wfile.write('<p><b>Hello, world!</b></p>'.encode('utf-8'))
+        self.wfile.write('<p>Hello, world!</p>'.encode('utf-8'))
         feed = get_feed(self.path)
-        self.wfile.write(feed['feed']['title'].encode('utf-8'))
+        self.wfile.write(feed.encode('utf-8'))
         return
--- api/rss.py
+++ api/rss.py
@@ -3,10 +3,14 @@ from urllib.parse import urlparse,parse_qs
 
 
 def get_rss(url_param):
-    source = parse_qs(urlparse(url_param)[4])['source']
-    feed = get_feed_list(source[0])
-    # feed['feed']['title'] = feed['feed']['title']+"$$$"+str(urlparse(url_param))
-    return feed
+    try:
+        source = parse_qs(urlparse(url_param)[4])['source']
+    except Exception:
+        source = ["https://vvkhash.substack.com/feed"]
+    feed = ''
+    for s in source:
+        feed = feed + get_feed_list(s)['feed']['title']
+    return str(feed)
 
 
 def get_feed_list(url):
@@ -17,11 +21,9 @@ if __name__ == "__main__":
     # Local debug
     # https://www.pythonforbeginners.com/feedparser/using-feedparser-in-python
 
-    text = get_rss("https://vvkhash.substack.com/feed?source=https://vvkhash.substack.com/feed")
+    text = get_rss("https://vvkhash.substack.com/feed?source=https://vvkhash.substack.com/feed&source=https://tty.vvkhash.com/data/rss")
     print(text)
     exit(0)
-
-
 
 
 
