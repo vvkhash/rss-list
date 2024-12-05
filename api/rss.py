162491e2baee1b1fc2ace25fb264e2af5dbab337
Valeriy Khashkovskiy <valeriy.khashkovskiy1@lseg.com>
2024-12-05 16:13:48 +0100

    select authored updates only

--- api/rss.py
+++ api/rss.py
@@ -13,12 +13,17 @@ def get_rss(url_param):
             font-size: smaller; \
             color: gray; \
         \" "
+        all_feed = get_feed_list(s)
         feed=""
-        for e in get_feed_list(s)['entries']:
-            date_list=e['published'].split(" ")
-            date = f"{date_list[1]} {date_list[2]} {date_list[3]}"
-            feed += f"<p style={blog_style}> <a href=\"{e['link']}\" target=\"_blank\" >{e['title']}</a> ({date}) </p>\n"
-        # feed = feed + get_feed_list(s)['feed']['title']
+        for e in all_feed['entries']:
+            author = ""
+            for key, value in e.items():
+                if key == "author":
+                    author = value
+            if author != "":
+                date_list=e['published'].split(" ")
+                date = f"{date_list[1]} {date_list[2]} {date_list[3]}"
+                feed += f"<p style={blog_style}> <a href=\"{e['link']}\" target=\"_blank\" >{e['title']}</a> ({date}) </p>\n"
     return str(feed)
 
 
@@ -30,7 +35,10 @@ if __name__ == "__main__":
     # Local debug
     # https://www.pythonforbeginners.com/feedparser/using-feedparser-in-python
 
-    text = get_rss("https://blog.vvkhash.com/rss.xml?source=https://blog.vvkhash.com/rss.xml")
+    # text = get_rss("https://blog.vvkhash.com/rss.xml?source=https://blog.vvkhash.com/rss.xml")
+    text = get_rss("https://blog.vvkhash.com/rss.xml?source=http://localhost:8000/rss.xml")
+    # text = get_rss("https://blog.vvkhash.com/rss.xml?source=http://localhost:8000/feed.json")
+
     print(text)
     exit(0)
 
